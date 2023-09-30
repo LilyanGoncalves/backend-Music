24b766262dd34d25902e6c0331562514e6bf9d32
RenatoFernandoSilvaGoncalves <94020148+RenatoFernandoSilvaGoncalves@users.noreply.github.com>
2023-09-30 10:21:06 -0300

    Update conexao.js (professor Renato) para apontar para o banco de dados da infra cloudpanel

--- database/conexao.js
+++ database/conexao.js
@@ -6,22 +6,22 @@ export default async function conectar(){
         return global.conexao;
     }
 
-    // const conexao = await mysql.createConnection({
-    //     host:"localhost",
-    //     user:"aluno29-pfsii",
-    //     port:"3306",
-    //     password:"F7eI8uwrDzPyQcUqHDWC",
-    //     database:"injmusic-bd"
-    // });
-
-
     const conexao = await mysql.createConnection({
+         host:"localhost",
+         user:"aluno29-pfsii",
+         port:"3306",
+         password:"F7eI8uwrDzPyQcUqHDWC",
+         database:"injmusic-bd"
+     });
+
+
+    /*const conexao = await mysql.createConnection({
         host:"localhost",
         user:"user",
         port:"3306",
         password:"123456",
         database:"injmusic-bd"
-    });
+    });*/
 
     global.conexao = conexao;
 
